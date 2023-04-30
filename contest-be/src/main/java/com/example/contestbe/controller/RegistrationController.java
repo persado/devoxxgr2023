baa1a91641750da34a52f70b5c03eb9577263688
Anna Parissi <anna.parissi@persado.com>
2023-04-30 16:51:43 +0300

    Rename ContestEntrantController

--- contest-be/src/main/java/com/example/contestbe/controller/RegistrationController.java
+++ contest-be/src/main/java/com/example/contestbe/controller/RegistrationController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
-public class ContestEntrantController {
+public class RegistrationController {
 
     private final ContestEntrantService contestEntrantService;
 
