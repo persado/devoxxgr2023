443d0483e4144776ed3a409133a22120a18f45b2
Anna Parissi <anna.parissi@persado.com>
2023-05-03 18:44:20 +0300

    Set Athens TimeZone

--- contest-be/src/main/java/com/example/contestbe/ContestBeApplication.java
+++ contest-be/src/main/java/com/example/contestbe/ContestBeApplication.java
@@ -3,10 +3,13 @@ package com.example.contestbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ContestBeApplication {
 
     public static void main(String[] args) {
+        java.util.TimeZone.setDefault(TimeZone.getTimeZone("Europe/Athens"));
         SpringApplication.run(ContestBeApplication.class, args);
     }
 
