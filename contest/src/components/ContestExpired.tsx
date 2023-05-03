b25ce32e98473a4a13e0294cd8b421d32512f526
Anna Parissi <anna.parissi@persado.com>
2023-05-03 14:48:25 +0300

    Update expiration page content

--- contest/src/components/ContestExpired.tsx
+++ contest/src/components/ContestExpired.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import {Row} from "antd";
+import {Col, Row, Space} from "antd";
 
 function ContestExpired() {
     return (
         <div>
             <Row justify="center">
-                <h1>The contest has now expired</h1>
+                <Col xs={24} xl={24} style={{textAlign: 'center', padding: 10}}>
+                        <h1>We would like to inform you that the contest has now officially expired</h1>
+                </Col>
             </Row>
+            <Row justify="center">
+                <Col xs={24} xl={8} style={{textAlign: 'center', padding: 10}}>
+                    <p>Thank you all for your enthusiastic participation!</p>
 
+                    <p>Find out more about our current career openings at &nbsp;
+                        <a href="https://apply.workable.com/persado/" target="_blank"
+                           style={{color: "#626f7c"}}>Careers at Persado</a>
+                    </p>
+                </Col>
+            </Row>
         </div>
 
     );
